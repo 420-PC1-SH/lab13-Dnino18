425de2ec939add78f492cd8de2615ed2a9ef3743
Dnino18 <122505784+Dnino18@users.noreply.github.com>
2024-07-19 13:16:11 -0400

    exercice 2 - requis 2 fail

--- greeting.test.js
+++ greeting.test.js
@@ -3,3 +3,10 @@ const greet = require('./greeting.js');
 test("Retour salutation avec le nom", function () {
     expect(greet('Luke')).toBe('Hello, Luke');
 });
+
+
+test("Lorsque undefined, null ou une chaîne de caractère vide est passé à la fonction renvoyer « Hello there »", function () {
+    expect(greet(undefined)).toBe('Hello there');
+    expect(greet(null)).toBe('Hello there');
+    expect(greet('')).toBe('Hello there');
+});
