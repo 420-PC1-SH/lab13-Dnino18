e55e29197717f5524317a43a90c86b53b9b41e8d
Dnino18 <122505784+Dnino18@users.noreply.github.com>
2024-07-19 13:08:55 -0400

    exercice 2 - requis 1 pass

--- greeting.test.js
+++ greeting.test.js
@@ -1,4 +1,4 @@
-const processor = require('./greeting.js');
+const greet = require('./greeting.js');
 
 test("Retour salutation avec le nom", function () {
     expect(greet('Luke')).toBe('Hello, Luke');
